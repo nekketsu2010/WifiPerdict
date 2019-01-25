9d3afa6cefff481c7085da97e3a5a8c14e06ccf6
nekketsu2010 <41464917+nekketsu2010@users.noreply.github.com>
2019-01-25 12:52:00 +0900

    Addseparate.py

--- WaveCut.py
+++ WaveCut.py
@@ -1,8 +1,17 @@
+import os
+
 import librosa
+import pandas as pd
+import numpy as np
 
-fileName = "train/" + ""
+meta_data = pd.read_table("class_train.tsv")
+x = list(meta_data.loc[:, "fileName"])
+if not os.path.exists("trainAfter"):
+    os.mkdir("trainAfter")
+for i in range(len(x)):
+    fileName = "train/" + x[i] + ".wav"
 
-wav = librosa.load(fileName)
-wav = librosa.effects.trim(wav)
-
-librosa.output.write_wav("test.wav", wav, norm=True)
+    wav, sr = librosa.load(fileName)
+    wav, index = librosa.effects.trim(wav, top_db=30)
+    print(str(i) + "個のデータ処理完了")
+    librosa.output.write_wav("trainAfter/" + x[i] + ".wav", wav,sr=sr, norm=True)
