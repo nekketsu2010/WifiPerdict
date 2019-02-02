5bb23382b5d622456717ecb911fccceb971de489
nekketsu2010 <41464917+nekketsu2010@users.noreply.github.com>
2019-02-02 13:39:04 +0900

    pythonデータ減らした
    次は性別と年代に分けて学習する

--- WavToImage.py
+++ WavToImage.py
@@ -9,63 +9,42 @@ import matplotlib.pyplot as plt
 # load a wave data
 from sklearn import preprocessing
 
-
 def load_wave_data(audio_dir, file_name):
     file_path = os.path.join(audio_dir, file_name)
     x, fs = librosa.load(file_path, sr=16000)
     return x, fs
 
-# change wave data to mel-stft
-def calculate_melsp(x, filename, num, n_fft=1024, hop_length=128):
-    stft = np.abs(librosa.stft(x, n_fft=n_fft, hop_length=hop_length))**2
-    log_stft = librosa.power_to_db(stft)
-    melsp = librosa.feature.melspectrogram(S=log_stft,n_mels=128)
-    melsp = preprocessing.scale(melsp, axis=1)
-    # mfcc = librosa.feature.mfcc(x, sr=16000, n_mfcc=128)
-    # spec = librosa.feature.melspectrogram(x, sr=16000)
-    # librosa.display.specshow(mfcc, sr=16000)
-    librosa.display.specshow(melsp, sr=16000)
-    plt.savefig(str(num) + "回目/Image/" + filename + ".png", facecolor="azure", bbox_inches='tight', pad_inches=0)
-    plt.close()
-    # return melsp
-    # return mfcc
+def main(num=0, train=True):
+    tsvname = 'sample_submit.tsv'
+    audioFolder = 'silentTest'
+    imageFolder = 'TestImage'
+    if train:
+        tsvname = 'class_train.tsv'
+        audioFolder = 'silentTrain'
+        imageFolder = 'Image'
 
-def save_np_data(num, x, y, aug=None, rates=None):
+    meta_data = pd.read_table(tsvname)
 
-    for i in range(len(y)):
-        if not os.path.exists("trainAfter/" + x[i] + ".wav"):
+    x = list(meta_data.loc[:, "fileName"])
+    # save test dataset
+    for i in range(len(x)):
+        if not os.path.exists(audioFolder + "/" + x[i] + ".wav"):
             print("こいつない")
             print(x[i])
-            # exit()
             continue
-        if os.path.exists(str(num) + "回目/Image/" + x[i] + ".png"):
+        if os.path.exists(str(num) + "回目/" + imageFolder + "/" + x[i] + ".png"):
             continue
-        _x, fs = load_wave_data("trainAfter", x[i] + ".wav")
-        if aug is not None:
-            _x = aug(x=_x, rate=rates[i])
+        _x, fs = load_wave_data(audioFolder, x[i] + ".wav")
         print(str(i) + "個処理しました！")
-        _x = calculate_melsp(_x, x[i], num)
-    #     print(_x.shape)
-    #     np_data[i] = _x
-    #     np_targets[i] = y[i]
-    # np.savez(filename, x=np_data, y=np_targets)
-
-def main(num=5):
-    meta_data = pd.read_table("class_train.tsv")
-    labels, uniques = pd.factorize(meta_data['target'])
-    meta_data['target'] = labels
-    print(meta_data)
-
-    data_size = meta_data.shape
-    # arrange target label and its name
-    class_dict = meta_data["target"].unique()
-    print(class_dict)
-
-    # get training dataset and target dataset
-    x = list(meta_data.loc[:, "fileName"])
-    y = list(meta_data.loc[:, "target"])
-    # save test dataset
-    if not os.path.exists("esc_melsp_test.npz"):
-        save_np_data(num, x, y)
+        #ここからメルスペクトル変換処理
+        n_fft = 1024
+        hop_length = 128
+        stft = np.abs(librosa.stft(x, n_fft=n_fft, hop_length=hop_length)) ** 2
+        log_stft = librosa.power_to_db(stft)
+        melsp = librosa.feature.melspectrogram(S=log_stft)
+        mfccs = librosa.feature.mfcc(S=melsp)
+        mfccs = preprocessing.scale(mfccs, axis=1)
+        librosa.display.specshow(mfccs, sr=16000)
+        plt.savefig(str(num) + "回目/Image/" + x[i] + ".png", facecolor="azure", bbox_inches='tight', pad_inches=0)
+        plt.close()
     print("終わったお")
-main()
