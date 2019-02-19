99f304941e11680001a6da7d1f8fe469b19b3605
nekketsu2010 <41464917+nekketsu2010@users.noreply.github.com>
2019-02-19 20:41:54 +0900

    痛恨のミス
    かさ増しできてなかった

--- WavToImage.py
+++ WavToImage.py
@@ -50,7 +50,7 @@ def main(num=0, train=True):
     n_fft = 4096
     hop_length = 1024  # n_fftとってきてhop_lenghtだけずらしてn_fftだけポイントを取ってくる
     for image_num in range(len(imageFolders)):
-        if image_num == 0 or image_num == 1 or image_num == 2:
+        if image_num == 0:
             continue
         imageFolder = imageFolders[image_num]
         for i in range(len(x)):
@@ -63,14 +63,14 @@ def main(num=0, train=True):
             _x, fs = load_wave_data(audioFolder, x[i] + ".wav")
 
             #ここで，numの値に応じたノイズを加える（num>0なら）
-            if num == 1:
+            if image_num == 1:
                 rates = random.randint(1, 50) / 10000
                 _x = add_white_noise(_x, rate=rates)
-            elif num == 2:
+            elif image_num == 2:
                 _x = shift_sound(_x, rate=random.randint(2, 6))
-            elif num == 3:
+            elif image_num == 3:
                 _x = stretch_sound(_x, rate=random.randint(80, 120)/100)
-            elif num == 4:
+            elif image_num == 4:
                 rates = random.randint(1, 50) / 10000
                 _x = add_white_noise(_x, rate=rates)
                 if random.choice([True, False]):
@@ -81,8 +81,10 @@ def main(num=0, train=True):
             #ここからメルスペクトル変換処理
             stft = np.abs(librosa.stft(_x, n_fft=n_fft, hop_length=hop_length)) ** 2
             log_stft = librosa.power_to_db(stft)
+            # mfccs = librosa.feature.mfcc(S=log_stft, sr=8000)
             librosa.display.specshow(log_stft, sr=8000)
             plt.savefig(str(num) + "回目/" + imageFolder + "/" + x[i] + ".png", facecolor="azure", bbox_inches='tight', pad_inches=0)
+            # plt.show()
             plt.close()
             # 二枚目が一番わかり易い↑
             # melsp = librosa.feature.melspectrogram(S=log_stft)
