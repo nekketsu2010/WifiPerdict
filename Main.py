533d92ac03655bff2e7ba775c3019171f8ac7c17
itoki12 <zxxgxdfggd@gmail.com>
2019-02-19 21:13:48 +0900

    KPC
    melはここで

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import sys
 import os
 import WavToImage
 import PictureToNumpy
-import PictureTraining
-import NewPictureTraining
+# import PictureTraining
+# import NewPictureTraining
 
 number = sys.argv[1]
 #一通りフォルダ作成
@@ -22,8 +22,8 @@ if not os.path.exists(str(top) + "/Model"):
     os.mkdir(str(top) + "/Model")
 
 WavToImage.main(number, train=True)
-# WavToImage.main(number, train=False)
+WavToImage.main(number, train=False)
 PictureToNumpy.main(number, train=True)
-# PictureToNumpy.main(number, train=False)
+PictureToNumpy.main(number, train=False)
 # PictureTraining.main(number)
 # NewPictureTraining.main(number)
